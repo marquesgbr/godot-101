240f330c9c42fce07b8ed72fe4416c3abe0d0da6
marquesgbr <gma5@cin.ufpe.br>
2025-06-22 18:13:42 -0300

    fix: last fixes on gem counter

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,10 +3,18 @@ extends CanvasLayer
 @onready var label = $Label
 
 var count := 0 : set = _set_count
+var total := 0 : set = _set_total
 
 func _set_count(value):
 	count = value
-	label.text = "Gemas coletadas: %d" % count
+	_update_label()
+
+func _set_total(value):
+	total = value
+	_update_label()
+
+func _update_label():
+	label.text = "Gemas coletadas: %d / %d" % [count, total]
 
 func show_win_message():
-	label.text = "Parabéns! Todos as gemas foram coletadas!"
+	label.text = "Parabéns! Todas as gemas foram coletadas!"
--- scripts/main.gd
+++ scripts/main.gd
@@ -11,12 +11,16 @@ func _ready():
 	randomize()
 	total_items = randi_range(10, 35)
 	print("Total de gemas: ", total_items)
-	for i in total_items:
+	hud.total = total_items
+	hud.count = 0
+	
+	for i in range(total_items):
 		spawn_item()
 
 func spawn_item():
 	var item = item_scene.instantiate()
 	var safe_margin = 200  # margem longe do player
+	
 	item.position = Vector2(
 		randf_range(safe_margin, 1280 - safe_margin),
 		randf_range(safe_margin, 720 - safe_margin)
